0def67c12e3f4d637dca4a7ae1e7435b3d9fd9ed
camu-al <alexammar04@gmail.com>
2025-05-13 11:57:00 +0200

    Hola mon camu compartit

--- src/com/daw/prova/HolaMonCompartit.java
+++ src/com/daw/prova/HolaMonCompartit.java
@@ -5,6 +5,7 @@ public class HolaMonCompartit {
 	public static void main(String[] args) {
 		System.out.println("Hola Món Raül 01!");
 		System.out.println("Hola Món Raül 05!");
+		System.out.println("Hola Món Camu 06!");
 	}
 
 }
