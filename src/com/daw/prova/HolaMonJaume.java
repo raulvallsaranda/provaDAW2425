bba8364433ec62b011dd1f1dca95524afaa864f0
pepinillos00 <tursisternes53@gmail.com>
2025-05-13 11:57:50 +0200

    Hola Mon1

---
+++ src/com/daw/prova/HolaMonJaume.java
@@ -0,0 +1,10 @@
+package com.daw.prova;
+
+public class HolaMonJaume {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mon Jaume");
+	}
+
+}
