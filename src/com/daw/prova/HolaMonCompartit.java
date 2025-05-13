5f3daa175bfbbf97396f566fa0b7d76737c2408d
Samuel Enguix Carbó <samuelenguixcarbo@gmail.com>
2025-05-13 12:01:20 +0200

    Compartit Samuel

--- src/com/daw/prova/HolaMonCompartit.java
+++ src/com/daw/prova/HolaMonCompartit.java
@@ -4,6 +4,7 @@ public class HolaMonCompartit {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Món Raül 01!");
+		System.out.println("Hola Mon Samuel 02");
 		System.out.println("Hola Món Helena 09");
 		System.out.println("Hola Mon Patryk 12");
 		System.out.println("Hola Món Yordan 13!");
